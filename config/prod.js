4e1d76314014b810fa74ddc0b1fa83300dcb23bf
adi_peled <adipeled499@gmail.com>
2020-07-24 16:18:19 +0300

    j

--- config/prod.js
+++ config/prod.js
@@ -1,3 +1,4 @@
 module.exports = {
-    "dbURL":" mongodb+srv://adi_peled:<adi>@cluster0.l1yta.mongodb.net/carDB?retryWrites=true&w=majority"
+    "dbURL": " mongodb+srv://adi_peled:adi@cluster0.l1yta.mongodb.net/carDB?retryWrites=true&w=majority"
+
 }
